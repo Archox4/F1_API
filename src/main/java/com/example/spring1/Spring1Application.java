d6b5178df4f1e3ef29bb3e2d5558f88ea5a7ac0f
Archox4 <123patryk890@gmail.com>
2026-07-07 13:15:32 +0200

    moving cross-origin to env and properties

--- src/main/java/com/example/spring1/Spring1Application.java
+++ src/main/java/com/example/spring1/Spring1Application.java
@@ -1,5 +1,6 @@
 package com.example.spring1;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "${frontend.host}")
 public class Spring1Application {
 
     static void main(String[] args) {
